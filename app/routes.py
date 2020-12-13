fc3789aed1603fbd5a3d2b3d943a7ff848c45eaf
arthurliu <theonlyarthurliu@gmail.com>
2020-12-12 19:44:33 -0800

    template rendering

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    data = [
+        {
+            'author': {'username': 'arthur'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'taylor'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'daniel'},
+            'body': 'i watned to play pingpong!'
+        }
+    ]
+    return render_template('index.html', title='Home', data=data)
