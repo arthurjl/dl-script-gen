0bbf171bc1f4519cab7f3c56b989a81f9abc5495
Daniel Zhu <dzhu99@cs.washington.edu>
2020-12-17 12:56:11 -0800

    update ngrok

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ def index():
     # Generate some text with the data and the audio clip
     if data:
         text_data = {"seedWords": data["seedWords"], "model": data["languageModel"]}
-        res = requests.post("https://63332a7ef8f3.ngrok.io/generateText", data=text_data)
+        res = requests.post("https://c709c4f714d4.ngrok.io/generateText", data=text_data)
         import sys
         print(res.text, file=sys.stderr)
         print(res.json(), file=sys.stderr)
@@ -18,7 +18,7 @@ def index():
         text = text_raw.split("\n")
 
         vis_data = {"text": text_raw, "model": data["languageModel"]}
-        res = requests.post("https://63332a7ef8f3.ngrok.io/generateCellVis", data=vis_data)
+        res = requests.post("https://c709c4f714d4.ngrok.io/generateCellVis", data=vis_data)
         visualizations = res.json()["cell_vis"]
 
         audio = "./static/out289.wav"
