914daebd575629f85cae9003fec2e592166fe91e
Daniel Zhu <dzhu99@cs.washington.edu>
2020-12-17 21:07:54 -0800

    update ngrok

--- app/routes.py
+++ app/routes.py
@@ -10,12 +10,8 @@ def index():
     # Generate some text with the data and the audio clip
     if data and data["seedWords"]:
         text_data = {"seedWords": data["seedWords"], "model": data["languageModel"]}
-<<<<<<< HEAD
-        res = requests.post("https://c709c4f714d4.ngrok.io/generateText", data=text_data)
+        res = requests.post("https://68cd937e2a67.ngrok.io/generateText", data=text_data)
         import sys
-=======
-        res = requests.post("https://63332a7ef8f3.ngrok.io/generateText", data=text_data)
->>>>>>> b07017925762b7b809298d73a3649e33183996a3
         print(res.text, file=sys.stderr)
         if (res.status_code != 200):
             return render_template('index.html', error_message="Unable to retrieve successful request from backend")
@@ -25,7 +21,7 @@ def index():
         text = text_raw.split("\n")
 
         vis_data = {"text": text_raw, "model": data["languageModel"]}
-        res = requests.post("https://c709c4f714d4.ngrok.io/generateCellVis", data=vis_data)
+        res = requests.post("https://68cd937e2a67.ngrok.io/generateCellVis", data=vis_data)
         visualizations = res.json()["cell_vis"]
 
         audio = "./static/out289.wav"
